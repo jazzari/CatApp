f6e62ef6ce260d2ba389ffeeeae3966552113a0c
jazzari <passionjazz@gmail.com>
2021-03-08 19:02:44 +0100

    TDD setting other routes to breeds controller

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,4 +1,10 @@
 class BreedsController < ApplicationController
     def index 
     end
+
+    def show
+    end
+
+    def destroy
+    end
 end
--- spec/routing/breeds_spec.rb
+++ spec/routing/breeds_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe '/breeds routes' do
     it "routes to breeds#index" do 
         expect(get '/breeds').to route_to('breeds#index')
     end
+
+    it "routes to breeds#show" do 
+        expect(get '/breeds/1').to route_to('breeds#show', id: '1')
+    end
+
+    it "routes to breeds#destroy" do 
+        expect(delete 'breeds/1').to route_to('breeds#destroy', id: '1')
+    end
 end
